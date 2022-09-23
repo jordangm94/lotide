7fce62c383df651522ed74eef4016d539af1c6b4
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-23 02:52:50 +0000

    Function to find and return the key of the first truthy value

---
+++ findKey.js
@@ -0,0 +1,62 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  let results = "";
+  for (let keys in object) {
+    if (callback(object[keys])) {
+      results = keys;
+      break;
+    } else {
+      undefined;
+    }
+  }
+  return results;
+};
+
+const results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(results1);
+
+const results2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "Akaleri"
+
+console.log(results2);
+
+const results3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1) // => "Blue Hill"
+
+console.log(results3)
+
+assertEqual(results1, "noma");
+assertEqual(results2, "Akaleri");
+assertEqual(results3, "Blue Hill");
+
+
+
+
+
