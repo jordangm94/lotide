0402c7a503e27c84a0ad850c93bd2e795eafa1f9
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-26 17:53:02 +0000

    New folder to hold tests that we will run our functions against, that way when calling function test results don't appear

---
+++ tests/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Call of duty", "Battlefield");
+assertEqual("Pizza", "Pizza");
+assertEqual(5, 5);
+assertEqual(4, 5);
