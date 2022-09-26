eabb62d60a140091232028de696b91b3596f3cae
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-26 21:10:17 +0000

    Adjusted name of function and file to be Objects not Object

---
+++ assertObjectsEqual.js
@@ -0,0 +1,17 @@
+const eqObjects = require('./eqObjects');
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+
+// assertObjectsEqual(ab, ba);
+
+module.exports = assertObjectsEqual;
