adb2da62f7be13528f887a61ad4957831f306541
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-26 20:17:56 +0000

    Removed handmade assertions and implemented mocha and chai testing framework

--- tests/tailTest.js
+++ tests/tailTest.js
@@ -2,25 +2,16 @@ const assertEqual = require('../assertEqual');
 
 const tail = require('../tail');
 
-//Testing for one elemnet in array and empty array: Should produce empty array!
+const assert = require('chai').assert;
 
-console.log(tail([2]));
-console.log(tail([]));
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-//Test Case 2: Check the returned array elements
-
-const result2 = tail(["Vanguard", "Cold War", "MW2"]);
-assertEqual(result2.length, 2);
-assertEqual(result2[0], "Cold War");
-assertEqual(result2[1], "MW2");
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns an empty array if nothing is inputted in the array []", () => {
+    assert.deepEqual(assertEqual(tail([]), []));
+  });
+  it("returns an empty array if nothing is inputted in the array []", () => {
+    assert.deepEqual(assertEqual(tail([2]), []));
+  });
+  it("If incoming array is populated, returns a new array with everything but the first element", () => {
+    assert.deepEqual(assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]));
+  });
+});
