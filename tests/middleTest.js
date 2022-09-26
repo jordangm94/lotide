71bceebdfe3a38ce3dae0702fe1192b6907ffd3d
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-26 20:17:35 +0000

    Removed handmade assertions and implemented mocha and chai testing framework

--- tests/middleTest.js
+++ tests/middleTest.js
@@ -2,14 +2,16 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 const middle = require('../middle');
 
-//Console.log tests for odd number arrays and array less than 2.
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log("---------")
-// Test assertions for if array has more than 2 numbers, and array.length is event
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("Return empty array if array has less than 2 numbers", () => {
+    assert.deepEqual(assertArraysEqual(middle([1, 2]), []));
+  });
+  it("If array of odd numbers, always return the middle number", () => {
+    assert.deepEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+  });
+  it("If array of even numbers, return the TWO middle number", () => {
+    assert.deepEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+  });
+});
